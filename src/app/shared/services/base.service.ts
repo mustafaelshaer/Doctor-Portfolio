e96559016f0ac85cf536c4cec13eb8b03c0d85ee
Mustafa <Mustafa.ElShaer@outlook.com>
2025-02-23 21:35:10 +0200

    enhancments

--- src/app/shared/services/base.service.ts
+++ src/app/shared/services/base.service.ts
@@ -6,7 +6,7 @@ import { Injectable } from '@angular/core';
 export class BaseService {
 
   constructor() { }
-  doctorName:string = "John Smith" ;
-  specialty:string = "Health Specialist"
+  doctorName:string = "Samar Hatem" ;
+  specialty:string = "Dentist"
 
 }
